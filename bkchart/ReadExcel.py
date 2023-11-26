8142e24e246a3ceaf2a2ec1dece2a6e6eae684f1
TungVo <vomtung@gmail.com>
2023-11-26 15:51:21 +0700

    girvann

---
+++ bkchart/ReadExcel.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+# Get the absolute path to the project's root directory
+project_root = os.path.dirname(__file__)
+
+# Assuming your Excel file is in a subdirectory named 'data'
+excel_file_path = os.path.join(project_root, 'anonymisedData', 'courses.csv')
+
+# Read the Excel file into a pandas DataFrame
+df = pd.read_csv(excel_file_path)
+
+# Display the DataFrame
+print(df)
