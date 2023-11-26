0d173d62ff5077edc9fd8b365da0d5cb900d7ff1
TungVo <vomtung@gmail.com>
2023-11-26 16:40:42 +0700

    girvan newman

--- Service/ExcelService.py
+++ Service/ExcelService.py
@@ -25,7 +25,11 @@ class ExcelService:
         # Read the Excel file into a pandas DataFrame
         df = pd.read_csv(excel_file_path)
 
+        # Iterate over rowsi
+        for index, row in df.iterrows():
+            print(f"Index: {index}, code_module: {row['code_module']}, code_presentation: {row['code_presentation']}, id_student: {row['id_student']}")
+
         # Display the DataFrame
-        print(df)
+        #print(df)
 
 
